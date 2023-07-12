f22ff8aff6f2efac84b3374807928f1085f50dc7
Phumrapee Limpianchop <git@rayriffy.com>
2023-07-12 19:22:31 +0900

    chore: note to user

--- src/context/counterAtom.ts
+++ src/context/counterAtom.ts
@@ -1,6 +1,8 @@
 import { atom } from 'nanostores'
 import { persistentAtom } from '@nanostores/persistent'
 
+// NOTE: comment L7-L10, and uncomment L6 to see normal behavior of an atom
+
 // export const counterAtom = atom<number>(0)
 export const counterAtom = persistentAtom<number>('hellooo', 0, {
   encode: JSON.stringify,
